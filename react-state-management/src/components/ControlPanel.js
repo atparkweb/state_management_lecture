9044c10e8a7c361b28ae3f98e613dff16afe36c8
Andrew Park <atpark333@gmail.com>
2021-12-08 21:52:51 +0900

    feat: implement attacks

--- react-state-management/src/components/ControlPanel.js
+++ react-state-management/src/components/ControlPanel.js
@@ -1,28 +1,27 @@
 import { useState } from "react";
 
-function ControlPanel({ players, onAttack }) {
-  const [selectedPlayer, setSelectedPlayer] = useState(null);
+function ControlPanel({ players, onAttack, miss }) {
+  const [selectedPlayer, setSelectedPlayer] = useState(players[0].id);
 
   const handleClick = (e) => {
-    if (selectedPlayer !== null) {
-      onAttack(selectedPlayer);
-    }
+    onAttack(selectedPlayer);
   };
 
   const handleChange = (e) => {
-    const selected = e.target.value;
-    console.log(e);
-    setSelectedPlayer(players[selected]);
+    setSelectedPlayer(parseInt(e.target.value));
   };
 
   return (
     <div className="control-panel">
       <select onChange={handleChange}>
         {players.map(p => {
-          return <option key={p.id} value="p.id">{p.name}</option>
+          return <option key={p.id} value={p.id}>{p.name}</option>
         })}
       </select>
-      <button onClick={handleClick}>Attack</button>
+      <button onClick={handleClick} disabled={miss}>Attack</button>
+      <div className="control-panel-alert">
+        {miss ? <span>MISSED!</span> : null}
+      </div>
     </div>
   )
 }
