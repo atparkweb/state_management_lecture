4b5cc3488473247c25d302303335731b7dcc5f84
Andrew Park <atpark333@gmail.com>
2021-12-08 21:22:53 +0900

    Add control panel to layout

--- react-state-management/src/App.js
+++ react-state-management/src/App.js
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import Character from "./components/Character";
-import { players } from './data/players';
+import ControlPanel from "./components/ControlPanel";
+import { getPlayers } from './data/players';
 
 function App() {
+  const [playerList, setPlayerList] = useState(getPlayers());
+
+  const handleAttack = (player) => {
+    // TODO: Implement
+  };
+
+  const handleRemove = (player) => {
+    // TODO: Implement
+  };
+
   return (
     <div className="app">
-      {players.map(p => {
-        return (
-          <div className="character-list" key={p.name}>
-            <Character data={p} />
-          </div>
-        )
-      })}
+      <header className="app-header">
+        <ControlPanel players={playerList} onAttack={handleAttack} onRemove={handleRemove} />
+      </header>
+
+      <main className="app-main">
+        {playerList.map(p => {
+          return (
+            <div className="character-list" key={p.name}>
+              <Character data={p} />
+            </div>
+          )
+        })}
+      </main>
     </div>
   );
 }
