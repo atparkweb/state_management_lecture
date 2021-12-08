6797035b0b88c74bd4c48247242187fa441866f9
Andrew Park <atpark333@gmail.com>
2021-12-08 15:19:59 +0900

    Add character data and style

--- react-state-management/src/App.js
+++ react-state-management/src/App.js
@@ -1,27 +1,16 @@
 import Character from "./components/Character";
+import { players } from './data/players';
 
 function App() {
-  const andy = {
-    name: "Brandalf",
-    race: "Half-orc",
-    klass: "Sorcerer",
-    level: 5,
-    alignment: "Chaotic Good",
-    background: "Soldier",
-    hp: 30,
-    attrs: {
-      str: { value: 8, mod: 0 },
-      int: { value: 15, mod: 0 },
-      wis: { value: 14, mod: 0 },
-      dex: { value: 8, mod: 0 },
-      cha: { value: 4, mod: 0 },
-      con: { value: 10, mod: 0 }
-    },
-  }
-
   return (
-    <div className="App">
-      <Character data={andy} />
+    <div className="app">
+      {players.map(p => {
+        return (
+          <div className="character-list" key={p.name}>
+            <Character data={p} />
+          </div>
+        )
+      })}
     </div>
   );
 }
