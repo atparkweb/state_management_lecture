3693be9612ee8337763e3af92e04720de61cd4af
Andrew Park <atpark333@gmail.com>
2021-12-08 21:22:33 +0900

    refactor: remove background from character card

--- react-state-management/src/components/Character.js
+++ react-state-management/src/components/Character.js
@@ -10,19 +10,20 @@ function Character({ data }) {
         <ul>
           <li>{race}</li>
           <li>{klass} {level}</li>
-          <li>{background}</li>
           <li>{alignment}</li>
         </ul>
       </header>
-      <section className="profile-card-section">
-        <h2>Attributes</h2>
-        <Attributes attributes={attrs} />
-      </section>
+
       <section className="profile-card-section">
         <h2>Status</h2>
         <ul>
           <li><abbr title="Health Points">HP</abbr>: {hp}</li>
         </ul>
+      </section>
+
+      <section className="profile-card-section">
+        <h2>Attributes</h2>
+        <Attributes attributes={attrs} />
       </section>
     </div>
   )
