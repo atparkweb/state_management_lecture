2d0f5327e18f7c16f06773d3f29db5c41ce2cae8
Andrew Park <atpark333@gmail.com>
2021-12-09 14:24:05 +0900

    Update styles

--- react-state-management/src/components/Battle.js
+++ react-state-management/src/components/Battle.js
@@ -1,36 +1,15 @@
 import { useState } from "react";
 import { getCharacters } from "../data/characters";
-import ControlPanel from "./ControlPanel";
 import CharacterSheet from "./CharacterSheet";
 
 function Battle() {
-  const [miss, setMiss] = useState(false);
   const [targetId, setTargetId] = useState(1);
   const [characters, setCharacters] = useState(getCharacters());
-
-  const handleAttack = (targetId) => {
-    setTargetId(targetId);
-    const newCharacters = characters.map(c => {
-      if (c.id === targetId) {
-        const damage = Math.round(Math.random());
-
-        if (damage === 0) {
-          setMiss(true);
-          setTimeout(() => setMiss(false), 500);
-        } else {
-          c.hp -= damage;
-        }
-      }
-      return c;
-    });
-
-    setCharacters(newCharacters);
-  };
 
   return (
     <>
       <header className="battle-header">
-        <ControlPanel characters={characters} onAttack={handleAttack} miss={miss} />
+        <h1 class="battler-header__heading">Battle Field</h1>
       </header>
 
       <main className="battle-main">
