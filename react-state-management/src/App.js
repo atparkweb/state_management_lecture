a618db288e8a0b9e411a1b5a8f2660a0485b3f79
Andrew Park <atpark333@gmail.com>
2021-12-09 13:40:38 +0900

    refactor: move battle logic into it's own component

--- react-state-management/src/App.js
+++ react-state-management/src/App.js
@@ -1,49 +1,9 @@
-import { useState } from "react";
-import CharacterSheet from "./components/CharacterSheet";
-import ControlPanel from "./components/ControlPanel";
-import { getCharacters } from './data/characters';
+import Battle from "./components/Battle";
 
 function App() {
-  const [characters, setCharacters] = useState(getCharacters());
-  const [miss, setMiss] = useState(false);
-  const [targetId, setTargetId] = useState(1);
-
-  const handleAttack = (targetId) => {
-    setTargetId(targetId);
-    const newCharacters = characters.map(c => {
-      if (c.id === targetId) {
-        const damage = Math.round(Math.random());
-
-        if (damage === 0) {
-          setMiss(true);
-          setTimeout(() => setMiss(false), 500);
-        } else {
-          c.hp -= damage;
-        }
-      }
-      return c;
-    });
-
-    setCharacters(newCharacters);
-  };
-
   return (
     <div className="app">
-      <header className="app-header">
-        <ControlPanel characters={characters} onAttack={handleAttack} miss={miss} />
-      </header>
-
-      <main className="app-main">
-        <div className="layout" >
-        {characters.map(c => {
-          return (
-            <div key={c.name} className="col">
-              <CharacterSheet character={c} target={targetId} />
-            </div>
-          )
-        })}
-        </div>
-      </main>
+      <Battle />
     </div>
   );
 }
