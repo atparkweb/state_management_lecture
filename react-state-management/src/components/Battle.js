183957b5c6b5cd90020da0260cec691de3e454ec
Andrew Park <atpark333@gmail.com>
2021-12-09 17:20:07 +0900

    feat: implement attack button

--- react-state-management/src/components/Battle.js
+++ react-state-management/src/components/Battle.js
@@ -3,13 +3,24 @@ import { getCharacters } from "../data/characters";
 import CharacterSheet from "./CharacterSheet";
 
 function Battle() {
-  const [targetId, setTargetId] = useState(1);
   const [characters, setCharacters] = useState(getCharacters());
+
+  const handleAttack = (id) => {
+    const newCharacters = characters.map(c => {
+      if (c.id === id) {
+	c.hp -= Math.round(Math.random() * 6);
+      }
+      
+      return c;
+    });
+    
+    setCharacters(newCharacters);
+  };
 
   return (
     <>
       <header className="battle-header">
-        <h1 class="battler-header__heading">Battle Field</h1>
+        <h1 className="battler-header__heading">Battle Field</h1>
       </header>
 
       <main className="battle-main">
@@ -17,7 +28,7 @@ function Battle() {
         {characters.map(c => {
           return (
             <div key={c.name} className="col">
-              <CharacterSheet character={c} target={targetId} />
+              <CharacterSheet character={c} onAttack={handleAttack} />
             </div>
           )
         })}
