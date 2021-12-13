054a277058888377638c24bd041a9b930375e7de
Andrew Park <atpark333@gmail.com>
2021-12-13 11:28:15 +0900

    feat: implement a common control panel

---
+++ react-state-management/src/components/ControlPanel.js
@@ -0,0 +1,12 @@
+// This is a stateless component
+
+function ControlPanel({ onAttack, msg }) {
+  return (
+    <div className="control-panel">
+      <button className="button control-panel__button" onClick={onAttack}>Attack</button>
+      { msg ? <div className="control-panel__alert">{msg}</div> : null }
+    </div>
+  )
+}
+
+export default ControlPanel;
