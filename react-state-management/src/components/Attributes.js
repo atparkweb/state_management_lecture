c34b54c31941e7c38016fd09cba0da5b1ab6bb97
Andrew Park <atpark333@gmail.com>
2021-12-15 11:45:41 +0900

    Clean up code style

--- react-state-management/src/components/Attributes.js
+++ react-state-management/src/components/Attributes.js
@@ -1,26 +1,27 @@
+import { getAttributes } from "../data/attributes";
 
-/* this is a state-less component */
 function Attributes({ attributes }) {
+  const attributeItems = (attrs) => {
+    let content = [];
+    const attributes = getAttributes();
+
+    for (const key in attributes) {
+      content.push(
+        <li key={key} className="attr-card-item">
+          <abbr title={attributes[key]} className="attr-card-key">{key}: </abbr>
+          <span className="attr-card-value">{attrs[key].value} ({attrs[key].mod})</span>
+        </li>
+      );
+    }
+
+    return content;
+  };
+
   return (
     <div className="attr-card">
       <ul>{attributeItems(attributes)}</ul>
     </div>
   );
 }
-
-function attributeItems(attributes) {
-  let content = [];
-
-  for (const key in attributes) {
-    content.push(
-        <li key={key} className="attr-card-item">
-        <span className="attr-card-key">{key}: </span>
-        <span className="attr-card-value">{attributes[key].value} ({attributes[key].mod})</span>
-        </li>
-    );
-  }
-
-  return content;
-};
 
 export default Attributes;
